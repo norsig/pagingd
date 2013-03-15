8366605485d21921df44c5a8fba6ea60492b8bac
Rick Ennis <ennis@dev1.(none)>
2013-03-15 12:35:29 -0400

    Added comments

--- get_oncall_status.pl
+++ get_oncall_status.pl
@@ -2,6 +2,10 @@
 
 use strict;
 
+# where is the paging daemon installed?
+my $PAGINGD = '/usr/local/bin/paging/pagingd';
+
+
 # sample script for use in a cron to email the weekly on call
 # schedule.  pipe output to mail
 
@@ -9,9 +13,12 @@ sub pagerd($;$) {
 	my $sGroup = shift;
 	my $iPlusDays = shift || 0;
 
-	return `/usr/local/bin/paging/pagingd oncall $sGroup $iPlusDays`;
+	return `$PAGINGD oncall $sGroup $iPlusDays`;
 }
 
+
+# here we hardcode some group names that we have oncall schedules for
+# we say we want to see who is oncall now and who is oncall 7 days from now
 
 print "STATION RELATIONS\n\n";
 print "Today:  \t" . pagerd('StationRelations');
