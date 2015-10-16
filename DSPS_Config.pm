47739f396279506c20330c2446130d04df92ca75
Rick Ennis <rennis@npr.org>
2015-10-16 14:30:49 -0400

    added support for multiple regex_subst lines in config fixed messagePostFixUp to stop removing all CRs

--- DSPS_Config.pm
+++ DSPS_Config.pm
@@ -23,7 +23,7 @@ our $sConfigPath = '/etc';
 my @aValueDirectives = (
     'default_maint',  'gateway_url',   'gateway_params', 'gateway_auth', 'fallback_email',       'nagios_recovery_regex', 'dsps_server',
     'smtp_server',    'server_listen', 'smtp_from',      'admin_email',          'rt_connection',         'override_user',
-    'override_regex', 'rt_link',       'log_rooms_to',   'nagios_problem_regex', 'http_auth', 'summary_text', 'subst_regex',
+    'override_regex', 'rt_link',       'log_rooms_to',   'nagios_problem_regex', 'http_auth', 'summary_text', 
 );
 my @aBoolDirectives = ('show_nonhuman', 'require_at', 'summary_reminder');
 tie my (%hSeenAliases), 'Hash::Case::Preserve';
@@ -613,6 +613,12 @@ sub readConfig(;$) {
                     next LINE;
                 }
             }
+        }
+
+        # regex substitution line
+        if (/\b(?:regex_subst)\s*:\s*\/*(.+)\/(.+?)\/*$/i) {
+            $g_hConfigOptions{regex_subst}{$1} = $2;
+            next;
         }
 
         # regex profile line
--- DSPS_Version.pm
+++ DSPS_Version.pm
@@ -5,6 +5,6 @@ use warnings;
 use base 'Exporter';
 our @EXPORT = ('$VERSION');
 
-our $VERSION = '3.1.34';
+our $VERSION = '3.1.35';
 
 1;
